373862f3980c666c9e6dc96bd81612d8bb65aa7c
lukedout5 <lukedout5@proton.me>
2024-09-30 15:31:55 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,3 +1,9 @@
+provider "google" {
+  project     = "my-project-id"
+  region      = "us-central1"
+}
+
+
 resource "google_storage_bucket" "static-site" {
   name          = "image-store.com"
   location      = "EU"
